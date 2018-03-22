ff912c493f970260ad1805997286913bac04ae02
tbricca <tbricca@gmail.com>
2018-03-22 11:35:18 -0700

    connected to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 import routes from './routes';
+
+mongoose.connect('mongodb://localhost:27017/devcoffee', () => {
+    console.log('Connected to Mongo');
+});
 
 const app = express();
 
