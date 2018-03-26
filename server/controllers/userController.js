7759f33c33a52ed367234b3acf1f61bf1e055b8c
tbricca <tbricca@gmail.com>
2018-03-26 10:20:12 -0700

    completed user save to mongodb database, and added the route into route file

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,14 +3,17 @@ import db from './../models';
 const userController = {};
 // whomever is using the pai will need to pass in 2 parameters (their username and password) which we get from the request body 
 userController.post = (req, res) => {
+    // This pulls the uername and password from our request body
     const { username, password } = req.body 
 
-    // Validation goes here
 
+    // Validation goes here
+// this creates the new instance of the user
     const user = new.db.User({
         username, 
         password
     });
+
 // this saves it to the database
     user.save().then((newUser) => {
         res.status(200).json({
--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,13 @@ import express from 'express';
 
 // Controller Imports /// 
 import basicController from './controllers/basicController';
+import userController from './controllers/userController';
 
 
 const routes = express();
-
+// Basic Routes
 routes.get('/', basicController.get)
  
+// User Routes
+routes.post('/signup, userController.post');
 export default routes;
