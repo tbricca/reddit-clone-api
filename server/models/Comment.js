6d91077243461e08c795ea2c28413fc4bef08682
tbricca <tbricca@gmail.com>
2018-03-29 09:44:44 -0700

    finished schema for comments, now adding in the controller

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -6,14 +6,23 @@ const { Schema } = mongoose;
 mongoose.Promise = global.Promise;
 
 const commentSchema = new Schema({
-    title: { type: String, required: true },
-    link: String,
-    text: String,
+    text: { type: String, required: true },
     isDeleted: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     _creator: { type: Schema.ObjectId, ref: 'User' },
+    _post: { type: Schema.ObjectId, ref: 'Post' },
+   
 });
 
-const Comment = mongoose.model('Post', commentSchema);
+const autoPopulateCreator = function(next) {
+    this.populate({
+        path: '_creator',
+        select: 'username created -_id '
+    });
+};
+
+commentSchema.pre('find', autoPopulateCreator);
+
+const Comment = mongoose.model('Comment', commentSchema);
 
 export default Comment;
--- server/models/Post.js
+++ server/models/Post.js
@@ -12,6 +12,7 @@ const postSchema = new Schema({
     isDeleted: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
     _creator: { type: Schema.ObjectId, ref: 'User' },
+    _comments: [{ type: Schema.ObjectId, ref: 'Comment' }],
 });
 
 const Post = mongoose.model('Post', postSchema);
